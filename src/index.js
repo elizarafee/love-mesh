0e08d00469d81a6e139e650826030fad10bb2e2a
Eliza Ahmed <elizarafee@gmail.com>
2020-06-08 02:06:01 +0600

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import $ from 'jquery'
+import Popper from 'popper.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
