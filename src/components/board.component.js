2889c205f5c7fca2309b97598f77ba55a6f9cd44
Eliza Ahmed <elizarafee@gmail.com>
2020-06-08 02:28:40 +0600

    static people component working

--- src/components/board.component.js
+++ src/components/board.component.js
@@ -1,23 +1,31 @@
-import React, { Component } from 'react'
-import People from './people.component'
+import React, { Component } from "react";
+import People from "./people.component";
 
 class Board extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-                 
-        }
-    }
+    this.state = {};
+  }
 
-    render() {
-        return (
-            <div>
-                <People />
-                <People />
+  render() {
+    return (
+      <div className="container mt-5 mb-5">
+        <div className="row justify-content-center">
+          <div className="col-lg-8 col-md-8">
+            <div className="row">
+              <People name='Eliza' background='bg-info' />
+              <People name='Tareque' background='bg-warning' />
             </div>
-        )
-    }
+            <div className="row">
+              <People name='Rahat' background='bg-success' />
+              <People name='Elina' background='bg-danger' />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Board;
--- src/components/people.component.js
+++ src/components/people.component.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const People = () => {
+const People = props => {
+
+    const background = props.background ? props.background : '';
+    const name = props.name ? props.name : 'People';
+
     return (
-        <div>
-            People
+        <div className={"col-sm-12 col-md-6 text-center p-5 " + background}>
+            { name }
         </div>
     )
 }
